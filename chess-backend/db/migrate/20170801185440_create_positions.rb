dc192f573fb754620cc11f3d2c9853c2feb4afec
peteryunkim <peteryunkim@gmail.com>
2017-08-01 14:18:12 -0500

    fixed default value null to nil for positions table

--- chess-backend/db/migrate/20170801185440_create_positions.rb
+++ chess-backend/db/migrate/20170801185440_create_positions.rb
@@ -1,9 +1,8 @@
 class CreatePositions < ActiveRecord::Migration[5.1]
   def change
     create_table :positions do |t|
-      t.integer :board_id
       t.string :position
-      t.integer :piece_id, :default=> NULL
+      t.integer :piece_id, :default=> nil
     end
   end
 end
