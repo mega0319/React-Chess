60647eba4facd60d0da1ca28bbddaa3445f5ea9e
peteryunkim <peteryunkim@gmail.com>
2017-08-08 14:59:48 -0400

    seeding pieces

--- chess-backend/db/seeds.rb
+++ chess-backend/db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def seed_pieces
+	pieces = ["king", "queen", "rook", "bishop", "knight", "pawn"]
+	pieces.each do |p|
+		case p
+		when "king"
+			Piece.create(name: "king", white: true)
+			Piece.create(name: "king", white: false)
+		when "queen"
+			Piece.create(name: "queen", white: true)
+			Piece.create(name: "queen", white: false)
+		when "rook"
+			2.times do 
+				Piece.create(name: "rook", white: true)
+				Piece.create(name: "rook", white: false)
+			end
+		when "bishop"
+			2.times do
+				Piece.create(name: "bishop", white: true)
+				Piece.create(name: "bishop", white: false)
+			end
+		when "knight"
+			2.times do
+				Piece.create(name: "knight", white: true)
+				Piece.create(name: "knight", white: false)
+			end
+		when "pawn"
+			8.times do
+				Piece.create(name: "bishop", white: true)
+				Piece.create(name: "bishop", white: false)
+			end
+	end
+end
